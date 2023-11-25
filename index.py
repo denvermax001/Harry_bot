26550ece5d6d977dd330c614b3e29820ed9a75c3
nishchal-kansara <119352064+nishchal-kansara@users.noreply.github.com>
2023-11-25 12:16:06 +0530

    Update index.py

--- index.py
+++ index.py
@@ -8,7 +8,7 @@ from telegram.ext import CommandHandler, MessageHandler, Filters, CallbackQueryH
 from movies_scraper import search_movies, get_movie
 
 TOKEN = os.getenv("TOKEN")
-URL = "https://flixseries-clone.vercel.app"
+URL = "https://flixseries-nkflix.koyeb.app"
 bot = Bot(TOKEN)
 
 def welcome_1(update, context) -> None:
