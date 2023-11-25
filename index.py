ed61e8da1bdcddbb9efdcecddad805e08a7437f1
nishchal-kansara <119352064+nishchal-kansara@users.noreply.github.com>
2023-11-25 17:35:17 +0530

    Update index.py

--- index.py
+++ index.py
@@ -11,17 +11,9 @@ TOKEN = os.getenv("TOKEN")
 URL = "https://flixseriesclone.vercel.app"
 bot = Bot(TOKEN)
 
-def welcome_1(update, context) -> None:
+def welcome(update, context) -> None:
     update.message.reply_text(f"Hello Dear, Welcome to NKFLIX.\n"
                               f"🔥 Download Your Favourite 🎬 Movies, Webseries & TV-Shows For 💯 Free And 🍿 Enjoy it.")
-    update.message.reply_text(f"Join Our Official Channel & Discussion Group:\n"
-                             f"Official Channel: @flixtvseries\n"
-                             f"Discussion Group: @flixtvseriesdiscussion\n"
-                             f"Join, Share & Support Us\n" 
-                             f"🤗 ADMIN NKFLIX 🤗")  
-    update.message.reply_text(f"Start searching using /search command.")
-
-def welcome_2(update, context) -> None:
     update.message.reply_text("👇 Enter 🎬 Movie, Web-Series Or TV-Show Name Below 👇")
 
 def find_movie(update, context):
@@ -36,7 +28,7 @@ def find_movie(update, context):
         reply_markup = InlineKeyboardMarkup(keyboards)
         search_results.edit_text('Results', reply_markup=reply_markup)
     else:
-        search_results.edit_text('Sorry 🥺, No result found!\nPlease retry Or contact admin.')
+        search_results.edit_text('Sorry 🙏, No result found!\nPlease retry Or contact admin.')
 
 def movie_result(update, context) -> None:
     query = update.callback_query
@@ -48,7 +40,7 @@ def movie_result(update, context) -> None:
     links = s["links"]
     for i in links:
         link += "🎬" + i + "\n" + links[i] + "\n\n"
-    caption = f"Direct Download 📥 Links 🔗:\n\n{link}"
+    caption = f"Direct Download Links:\n\n{link}"
     if len(caption) > 4095:
         for x in range(0, len(caption), 4095):
             query.message.reply_text(text=caption[x:x+4095])
@@ -58,8 +50,7 @@ def movie_result(update, context) -> None:
 def setup():
     update_queue = Queue()
     dispatcher = Dispatcher(bot, update_queue, use_context=True)
-    dispatcher.add_handler(CommandHandler('start', welcome_1))
-    dispatcher.add_handler(CommandHandler('search', welcome_2))
+    dispatcher.add_handler(CommandHandler('start', welcome))
     dispatcher.add_handler(MessageHandler(Filters.text, find_movie))
     dispatcher.add_handler(CallbackQueryHandler(movie_result))
     return dispatcher
